891c55f4d29878cbb89ea804d5bb454e1d956916
AnaTeresaQR <ana.quesada@ucrso.info>
2015-06-27 21:16:32 -0600

    validaciones para registrar
    no usa expresiones regulares, debe llamarse el js desde el body en el
    jsp para registro

---
+++ RegistroFix2/web/validacionesRegistro.js
@@ -0,0 +1,22 @@
+
+function formulario(form) {
+    if (form.nick.value == "") {
+        alert('El nick esta vacío');
+        form.nick.focus();
+        return false;
+    }
+    if (form.nombre.value == "") {
+        alert('El nombre esta vacío');
+        form.nombre.focus();
+        return false;
+    }
+    if (form.email.value == "") {
+        alert('El email no puede estar vacío');
+        form.email.focus();
+        return false;
+    }
+    return true;
+}
+
+
+
