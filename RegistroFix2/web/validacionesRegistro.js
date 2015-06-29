20c38ea83981f4e0efad38800a189410ff6cc9be
AnaTeresaQR <ana.quesada@ucrso.info>
2015-06-28 21:34:58 -0600

    más validaciones con expresiones regulares

--- RegistroFix2/web/validacionesRegistro.js
+++ RegistroFix2/web/validacionesRegistro.js
@@ -1,10 +1,6 @@
 function formulario(form) {
     var resultadoNick = validarNick(form);
-    if (form.email.value == "") {
-        alert('El email no puede estar vacío');
-        form.email.focus();
-        return false;
-    }
+
     if (form.password.value == "") {
         alert('La contraseña no puede estar vacía');
         form.password.focus();
@@ -20,6 +16,17 @@ function validarNick(form) {
     } else {
         alert('El nick esta vacío o contiene formato incorrecto');
         form.nick.focus();
+        return false;
+    }
+}
+function validarEmail(form) {
+    var expEmail = new RegExp("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+    if (expEmail.test(form.email.value)) {
+        alert('formato email correcto');
+        return true;
+    } else {
+        alert('El email esta vacío o contiene formato incorrecto');
+        form.email.focus();
         return false;
     }
 }
